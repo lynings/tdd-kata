cbfe12a32677ac526d644632d5bb2c4574ed6ba7
lyning <645464704@qq.com>
2019-04-07 02:03:00 +0800

    [TECH][lyning]重构单元测试

--- src/test/java/pers/lyning/kata/bowlinggame/GameTest.java
+++ src/test/java/pers/lyning/kata/bowlinggame/GameTest.java
@@ -1,25 +1,27 @@
 package pers.lyning.kata.bowlinggame;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
 
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void should_return_0_score_when_10_frames_knock_down_0_pins() throws Exception {
-        Game game = new Game();
-        int frames = 10;
-        int timesOfFrame = 2;
-        for (int num = 1; num <= frames * timesOfFrame; num++) {
-            game.roll(0);
-        }
+        rolls(20, 0);
         assertThat(game.getScore()).isEqualTo(0);
     }
 
     @Test
     public void should_return_90_score_when_each_frame_knock_down_9_pins() throws Exception {
-        Game game = new Game();
         int frames = 10;
         int timesOfFrame = 2;
         for (int num = 1; num <= frames * timesOfFrame; num++) {
@@ -34,20 +36,20 @@ public class GameTest {
 
     @Test
     public void should_return_300_score_when_each_frame_knock_down_10_pins_on_first_try() throws Exception {
-        Game game = new Game();
-        for (int num = 1; num <= 12; num++) {
-            game.roll(10);
-        }
+        rolls(12, 10);
         assertThat(game.getScore()).isEqualTo(300);
     }
 
     @Test
     public void should_return_155_score_when_each_frame_knock_down_10_pins_on_second_try() throws Exception {
-        Game game = new Game();
-        for (int num = 1; num <= 20; num++) {
-            game.roll(5);
-        }
-        game.roll(10);
+        rolls(20, 5);
+        rolls(1, 10);
         assertThat(game.getScore()).isEqualTo(155);
+    }
+
+    private void rolls(int times, int pins) {
+        for (int num = 1; num <= times; num++) {
+            game.roll(pins);
+        }
     }
 }
