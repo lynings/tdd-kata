beaa43c02de4373e863a1fc800720f194ef31a7a
lyning <645464704@qq.com>
2019-04-08 14:19:55 +0800

    [TECH][lyning]修改单元测试命名，提高可读性

--- src/test/java/pers/lyning/kata/christmaslights/ChristmasLightTest.java
+++ src/test/java/pers/lyning/kata/christmaslights/ChristmasLightTest.java
@@ -10,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ChristmasLightTest {
 
     @Test
-    public void should_return_9_when_turn_on_00_22() {
+    public void should_return_9_lights_when_turn_on_00_22() {
         ChristmasLight christmasLight = new ChristmasLight();
         Score score = Score.create().start(0, 0).end(2, 2);
         christmasLight.turnOn(score);
@@ -19,7 +19,7 @@ public class ChristmasLightTest {
     }
 
     @Test
-    public void should_return_5_when_turn_on_00_22_after_turn_off_11_22() {
+    public void should_return_5_lights_when_turn_on_00_22_after_turn_off_11_22() {
         ChristmasLight christmasLight = new ChristmasLight();
         christmasLight.turnOn(Score.create().start(0, 0).end(2, 2));
         christmasLight.turnOff(Score.create().start(1, 1).end(2, 2));
@@ -28,7 +28,7 @@ public class ChristmasLightTest {
     }
 
     @Test
-    public void should_return_4_when_turn_on_00_22_and_toggle_11_22_then_turn_off_00_22() {
+    public void should_return_4_lights_when_turn_on_00_22_and_toggle_11_22_then_turn_off_00_22() {
         ChristmasLight christmasLight = new ChristmasLight();
         christmasLight.turnOn(Score.create().start(0, 0).end(2, 2));
         christmasLight.toggle(Score.create().start(1, 1).end(2, 2));
@@ -38,7 +38,7 @@ public class ChristmasLightTest {
     }
 
     @Test
-    public void should_return_9_when_turn_on_00_22_and_toggle_11_22_then_turn_off_33_44() {
+    public void should_return_9_lights_when_turn_on_00_22_and_toggle_11_22_then_turn_off_33_44() {
         ChristmasLight christmasLight = new ChristmasLight();
         christmasLight.turnOn(Score.create().start(0, 0).end(2, 2));
         christmasLight.toggle(Score.create().start(1, 1).end(2, 2));
@@ -48,7 +48,7 @@ public class ChristmasLightTest {
     }
 
     @Test
-    public void should_return_13_when_turn_on_00_22_and_toggle_11_22() {
+    public void should_return_13_brightness_when_turn_on_00_22_and_toggle_11_22() {
         ChristmasLight christmasLight = new ChristmasLight();
         christmasLight.turnOn(Score.create().start(0, 0).end(2, 2));
         christmasLight.toggle(Score.create().start(1, 1).end(2, 2));
