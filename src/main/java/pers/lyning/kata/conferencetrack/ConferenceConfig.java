f20ce3a2c645c125cc39f0d32edd89a4d5d91c77
lyning <645464704@qq.com>
2019-04-10 02:02:19 +0800

    [TECH][lyning]将一些全局配置信息封装到ConferenceConfig

---
+++ src/main/java/pers/lyning/kata/conferencetrack/ConferenceConfig.java
@@ -0,0 +1,17 @@
+package pers.lyning.kata.conferencetrack;
+
+/**
+ * @author lyning
+ */
+public class ConferenceConfig {
+
+    public static final int MORNING_SESSION_DURATION_MINUTES = 180;
+
+    public static final int MORNING_SESSION_START_HOURS = 9;
+
+    public static final int AFTERNOON_SESSION_DURATION_MINUTES = 240;
+
+    public static final int AFTERNOON_SESSION_START_HOURS = 13;
+
+    public static final int LIGHTNING_DURATION_MINUTES = 5;
+}
