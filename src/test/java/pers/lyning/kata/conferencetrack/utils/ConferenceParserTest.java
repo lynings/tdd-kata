2be9c8fd1c2e2838d96838603ad0fa640a621183
lyning <645464704@qq.com>
2019-04-10 00:09:50 +0800

    [TECH][lyning]补充单元测试

---
+++ src/test/java/pers/lyning/kata/conferencetrack/utils/ConferenceParserTest.java
@@ -0,0 +1,23 @@
+package pers.lyning.kata.conferencetrack.utils;
+
+import org.junit.Test;
+import pers.lyning.kata.conferencetrack.ConferenceManagerTest;
+import pers.lyning.kata.conferencetrack.Talk;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConferenceParserTest {
+
+    @Test
+    public void parse() throws IOException {
+        File file = new File(ConferenceManagerTest.class.getResource("/conferencetrack/talk_list.txt").getPath());
+        List<Talk> talks = ConferenceParser.parse(new FileInputStream(file));
+        assertThat(talks).isNotEmpty();
+        assertThat(talks.size()).isEqualTo(19);
+    }
+}
