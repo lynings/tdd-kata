6aafcb57a2c543870c736967b1ad6ed50e2172d3
lyning <645464704@qq.com>
2019-04-11 10:56:20 +0800

    [TECH][lyning]增加一个通用的FileContentReader工具类

---
+++ src/main/java/pers/lyning/kata/utils/FileContentReader.java
@@ -0,0 +1,22 @@
+package pers.lyning.kata.utils;
+
+import java.io.*;
+
+/**
+ * @author lyning
+ */
+public class FileContentReader {
+
+    public static String getString(File file) throws IOException {
+        InputStream inputStream = new FileInputStream(file);
+        InputStreamReader reader = new InputStreamReader(inputStream, "UTF-8");
+
+        StringBuffer sb = new StringBuffer();
+        while (reader.ready()) {
+            sb.append((char) reader.read());
+        }
+        reader.close();
+        inputStream.close();
+        return sb.toString();
+    }
+}
