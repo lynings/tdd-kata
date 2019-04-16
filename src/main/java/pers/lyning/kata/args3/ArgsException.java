97b964b0f45157e65ab9bcc7d59210fb3c41301f
lyning <645464704@qq.com>
2019-04-16 14:33:23 +0800

    [TECH][lyning]练习Args第三遍

---
+++ src/main/java/pers/lyning/kata/args3/ArgsException.java
@@ -0,0 +1,11 @@
+package pers.lyning.kata.args3;
+
+/**
+ * @author lyning
+ */
+public class ArgsException extends RuntimeException {
+
+    public ArgsException(String message) {
+        super(message);
+    }
+}
