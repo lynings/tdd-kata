7eb032a86fb24037b9e64e83c76723003490075b
lyning <645464704@qq.com>
2019-04-16 09:05:24 +0800

    [TECH][lyning]重构MapValueParser

--- src/main/java/pers/lyning/kata/args/MapValueParser.java
+++ src/main/java/pers/lyning/kata/args/MapValueParser.java
@@ -1,8 +1,10 @@
 package pers.lyning.kata.args;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author lyning
@@ -14,16 +16,14 @@ public class MapValueParser implements ValueParser<Map<String, String>> {
         if (Objects.isNull(values)) {
             return new HashMap<>(0);
         }
-        String[] kvArr = values.split(",");
-        Map<String, String> map = new HashMap<>(kvArr.length);
         try {
-            for (String vkString : kvArr) {
-                String[] kv = vkString.split(":");
-                map.put(kv[0], kv[1]);
-            }
+            return Arrays
+                    .asList(values.split(","))
+                    .stream()
+                    .map(kv -> kv.split(":"))
+                    .collect(Collectors.toMap(a -> a[0], a -> a[1]));
         } catch (Exception e) {
             throw new ArgsException("The value of args must be an map，such as -m[&&] key1:val1,key2:val2,...");
         }
-        return map;
     }
 }
