f089357ddea2f85dd1769b16ea8e0f576392ad1b
lyning <645464704@qq.com>
2019-05-13 14:59:55 +0800

    [TECH][lyning]删除一些重复案例

--- src/main/java/pers/lyning/kata/fizzbuzz/Number.java
+++ src/main/java/pers/lyning/kata/fizzbuzz/Number.java
@@ -1,38 +1,26 @@
-package pers.lyning.kata.fizzbuzz;
+package pers.lyning.kata.fizzbuzz2;
 
 /**
  * @author lyning
  */
-public class Number {
+class Number {
     private Integer value;
 
-    public Number(Integer value) {
+    private Number(Integer value) {
         this.value = value;
     }
 
-    public boolean isFizz() {
-        return isDivisibleBy(3) || this.contains(3);
+    public static Number of(Integer number) {
+        return new Number(number);
     }
 
-
-    public boolean isBuzz() {
-        return isDivisibleBy(5) || this.contains(5);
-    }
-
-    public boolean isFizzBuzz() {
-        return isDivisibleBy(3) && isDivisibleBy(5)
-                || (isFizz() && this.isBuzz());
-    }
-
-    public Integer value() {
-        return this.value;
-    }
-
-    private boolean isDivisibleBy(Integer number) {
+    public boolean isDivisibleBy(Integer number) {
         return this.value % number == 0;
     }
 
-    private boolean contains(Integer number) {
-        return this.value.toString().contains(number.toString());
+    public boolean isContains(Integer number) {
+        return this.value
+                .toString()
+                .contains(number.toString());
     }
 }
