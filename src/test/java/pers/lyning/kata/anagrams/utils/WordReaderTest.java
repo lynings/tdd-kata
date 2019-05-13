620d147ef1936f75e12823199da5b0f3af130c01
lyning <645464704@qq.com>
2019-05-13 13:11:10 +0800

    [TECH][lyning]增加测试

---
+++ src/test/java/pers/lyning/kata/anagrams/utils/WordReaderTest.java
@@ -0,0 +1,34 @@
+package pers.lyning.kata.anagrams.utils;
+
+import org.junit.Test;
+import pers.lyning.kata.anagrams.Word;
+import pers.lyning.kata.testing.TestResourceFinder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WordReaderTest {
+
+    @Test
+    public void should_read_1633_words() throws IOException {
+        // given
+        File wordFile = TestResourceFinder.getFile("/anagrams/wordlist_1633.txt");
+        // when
+        Set<Word> words = WordReader.from(wordFile).asWords();
+        // then
+        assertThat(words.size()).isEqualTo(1633);
+    }
+
+    @Test
+    public void should_read_338882_words() throws IOException {
+        // given
+        File wordFile = TestResourceFinder.getFile("/anagrams/wordlist_338882.txt");
+        // when
+        Set<Word> words = WordReader.from(wordFile).asWords();
+        // then
+        assertThat(words.size()).isEqualTo(338882);
+    }
+}
