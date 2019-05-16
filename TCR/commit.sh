70ec314a68651f78524f378ce87197c9e0ee5f51
lyning <645464704@qq.com>
2019-05-16 17:03:04 +0800

    [TECH][lyning]tcr commit 阶段增加友好的提示功能

--- TCR/commit.sh
+++ TCR/commit.sh
@@ -1,2 +1,27 @@
 #!/usr/bin/env bash
-read -p "commit working: " name && git add . && git commit -m "$name"
+echo 'Commit Format: [TECH|FIX][Author]Working
+        TECH: Represents new code.
+        FIX: Represents fix the bug.
+
+Example:
+        [TECH][lyning]完成登录模块和单元测试
+        [FIX][lyning]修复登录模块验证码匹配空指针异常'
+
+# input tag
+tags=[TECH,FIX]
+until [[ "${tags[@]}" =~ "${tag}" ]]; do
+    read -p "TECH | FIX: " tag
+done
+
+# input author
+until [[ ! -z "${author}" ]]; do
+    read -p "Author: " author
+done
+
+# commit message
+until [[ ! -z "${msg}" ]]; do
+    read -p "Working: " msg
+done
+
+# add && commit
+git add . && git commit -m "[$tag][$author]$msg"
